46dc45f408b7e82f0d8d07cb6650efc01aac5f89
deveshsinghal22 <deveshsinghal22@gmail.com>
2018-04-09 15:18:19 +0530

    fixed issue for image url

--- type2/server/controllers/users.js
+++ type2/server/controllers/users.js
@@ -3,24 +3,24 @@ let router = express.Router();
 let responseHandler = require('../util/responseHandler').Response;
 let User = require('../models/User');
 
-router.get('/getMyProfile', function (req, res, next) {
-  User.findById(req.session.user._id).exec(function (err, user) {
-    if(!err) {
-      var responseObj = {
-        name : user.providerData.displayName,
-        email: user.email,
-        points: user.totalPoints,
-        gender: user.providerData.gender,
-        image: user.imaPoints geUrl
-      }
-    }
-    res.locals.responseObj = {
-      err: err,
-      data: responseObj,
-      msg: "users profile"
-    }
-    next();
-  })
-}, responseHandler)
+router.get('/getMyProfile', (req, res, next) => {
+    User.findById(req.session.user._id).exec(function(err, user) {
+        if (!err) {
+            let responseObj = {
+                name: user.providerData.displayName,
+                email: user.email,
+                points: user.totalPoints,
+                gender: user.providerData.gender,
+                image: user.imageUrl
+            }
+        }
+        res.locals.responseObj = {
+            err: err,
+            data: responseObj,
+            msg: "users profile"
+        };
+        next();
+    })
+}, responseHandler);
 
 module.exports = router;
